8c30599aecfa70f24b0f98b2373e9e77cb06bf54
so yeon <alicesysy@swu.ac.kr>
2021-12-02 14:48:21 +0900

    [add] color 코드 추가

--- Yakssok/Yakssok/Resources/Extensions/UIColor+.swift
+++ Yakssok/Yakssok/Resources/Extensions/UIColor+.swift
@@ -19,4 +19,8 @@ extension UIColor {
     @nonobjc class var subBlue: UIColor {
         return UIColor(red: 239.0 / 255.0, green: 242.0 / 255.0, blue: 248.0 / 255.0, alpha: 1.0)
     }
+    
+    @nonobjc class var tabBlue: UIColor {
+        return UIColor(red: 45.0 / 255.0, green: 58.0 / 255.0, blue: 89.0 / 255.0, alpha: 1.0)
+    }
 }
